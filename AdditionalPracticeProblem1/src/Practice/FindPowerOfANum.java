64d3bde9a004b07a1c98e862093bd0282879883e
rijulJain25 <102771948+rijulJain25@users.noreply.github.com>
2022-08-14 22:57:04 +0530

    10th file

---
+++ AdditionalPracticeProblem1/src/Practice/FindPowerOfANum.java
@@ -0,0 +1,50 @@
+/*Question:
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints:
+0 <= x <= 8 
+0 <= n <= 9
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32
+*/
+
+
+package Practice;
+
+import java.util.Scanner;
+
+public class FindPowerOfANum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x = s.nextInt();
+        if(n == 0 && x == 0){
+            System.out.println(1);
+            return;
+        }
+        if(x == 0){
+            System.out.println(1);
+            return;
+        }
+        
+        int pow = 1;
+        for(int i = 1; i<= x; i++){
+            pow = pow*n;
+        }
+        System.out.println(pow);
+	}
+
+}
---
+++ AdditionalPracticeProblem1/src/Practice/MultiplicationTable.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
